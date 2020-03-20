70d77a081a82184f140b5d43eb89eb4c7c061846
adam <20361733+rzencoder@users.noreply.github.com>
2020-03-20 22:36:40 +0000

    completed 1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -14,12 +14,19 @@ const App = ({ anecdotes }) => {
     setVotes(newVotes);
   };
 
+  const bestAnecdote = votes.indexOf(Math.max(...votes));
+  const bestTotal = Math.max(...votes);
+  console.log(bestTotal);
   return (
     <div>
+      <h2>Anecdote of the day</h2>
       <div>{anecdotes[selected]}</div>
       <div>has {votes[selected]} votes</div>
       <button onClick={handleVotes}>vote</button>
       <button onClick={handleNextAnecdote}>next anecdote</button>
+      <h2>Anecdote with the most votes</h2>
+      <div>{anecdotes[bestAnecdote]}</div>
+      <div>has {bestTotal} votes</div>
     </div>
   );
 };
