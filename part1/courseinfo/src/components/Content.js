f2d0599343340e6bdb05fd4207e9ee778a64eef3
adam <20361733+rzencoder@users.noreply.github.com>
2020-03-20 18:26:53 +0000

    completed parts 1.3-1.5

--- part1/courseinfo/src/components/Content.js
+++ part1/courseinfo/src/components/Content.js
@@ -2,13 +2,8 @@ import React from "react";
 import Part from "./Part";
 
 const Content = props => {
-  return (
-    <>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
-    </>
-  );
+  const parts = props.parts.map(part => <Part part={part} />);
+  return <>{parts}</>;
 };
 
 export default Content;
--- part1/courseinfo/src/components/Total.js
+++ part1/courseinfo/src/components/Total.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Total = (props) => {
-  return (
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-  );
+const Total = props => {
+  const exercises = props.parts.reduce((curr, val) => {
+    return curr + val.exercises;
+  }, 0);
+  return <p>Number of exercises {exercises}</p>;
 };
 
 export default Total;
