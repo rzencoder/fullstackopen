d91252384998d200076a0a7ed76fe96d072946a6
adam <20361733+rzencoder@users.noreply.github.com>
2020-03-20 22:22:14 +0000

    completed 1.13

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -2,15 +2,24 @@ import React, { useState } from "react";
 
 const App = ({ anecdotes }) => {
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
 
-  const handleClick = () => {
+  const handleNextAnecdote = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length));
+  };
+
+  const handleVotes = () => {
+    const newVotes = [...votes];
+    newVotes[selected]++;
+    setVotes(newVotes);
   };
 
   return (
     <div>
       <div>{anecdotes[selected]}</div>
-      <button onClick={handleClick}>next anecdote</button>
+      <div>has {votes[selected]} votes</div>
+      <button onClick={handleVotes}>vote</button>
+      <button onClick={handleNextAnecdote}>next anecdote</button>
     </div>
   );
 };
