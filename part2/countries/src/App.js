8eaa19dd169f74f7d2be4fedd463426bd8304c60
adam <20361733+rzencoder@users.noreply.github.com>
2020-03-21 19:14:56 +0000

    completed 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -4,6 +4,7 @@ import axios from "axios";
 function App() {
   const [countries, setCountries] = useState([]);
   const [newSearch, setNewSearch] = useState("");
+  const [showCountry, setShowCountry] = useState({});
 
   useEffect(() => {
     axios.get("https://restcountries.eu/rest/v2/all").then(res => {
@@ -13,6 +14,20 @@ function App() {
 
   const handleSearch = e => {
     setNewSearch(e.target.value);
+    setShowCountry({});
+  };
+
+  const getWeather = async country => {
+    let res = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${process.env.REACT_APP_KEY}`
+    );
+    return res.data;
+  };
+
+  const handleShowCountry = country => {
+    getWeather(country).then(data => {
+      setShowCountry({ ...country, weather: data });
+    });
   };
 
   const renderCountries = () => {
@@ -21,27 +36,35 @@ function App() {
     });
     if (filteredCountries.length > 10) {
       return <li>Too many matches, specify another filter</li>;
-    } else if (filteredCountries.length === 1) {
-      return renderCountry(filteredCountries[0]);
     } else {
       return filteredCountries.map(country => (
-        <div key={country.name}>{country.name}</div>
+        <div key={country.name}>
+          <div>{country.name}</div>
+          <button onClick={() => handleShowCountry(country)}>show</button>
+        </div>
       ));
     }
   };
 
-  const renderCountry = country => {
-    const languages = country.languages.map(language => {
+  const renderCountry = () => {
+    const languages = showCountry.languages.map(language => {
       return <li key={language.name}>{language.name}</li>;
     });
+    const temp = Math.round(showCountry.weather.main.temp - 273);
+    const weather = showCountry.weather.weather[0].main;
+    const wind = showCountry.weather.wind.speed;
     return (
       <div>
-        <h2>{country.name}</h2>
-        <p>capital: {country.capital}</p>
-        <p>population: {country.population}</p>
+        <h2>{showCountry.name}</h2>
+        <p>capital: {showCountry.capital}</p>
+        <p>population: {showCountry.population}</p>
         <h3>Languages</h3>
         <ul>{languages}</ul>
-        <img src={country.flag} alt={`Flag of ${country.name}`} />
+        <img src={showCountry.flag} alt={`Flag of ${showCountry.name}`} />
+        <h3>Weather in {showCountry.capital}</h3>
+        <div>temperature: {temp} C</div>
+        <div>weather: {weather}</div>
+        <div>wind speed: {wind}</div>
       </div>
     );
   };
@@ -53,6 +76,7 @@ function App() {
         <input onChange={handleSearch} value={newSearch} />
       </div>
       {countries.length && renderCountries()}
+      {showCountry.name && renderCountry()}
     </div>
   );
 }
