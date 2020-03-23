49068ace8092714f931c89f5d7c4c12d94abc60d
adam <20361733+rzencoder@users.noreply.github.com>
2020-03-23 14:31:38 +0000

    completed 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("body", function(req, res) {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
