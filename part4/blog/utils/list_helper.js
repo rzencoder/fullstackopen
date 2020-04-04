c319791a015c2a7983915613c50d432d50aa865c
adam <20361733+rzencoder@users.noreply.github.com>
2020-04-04 17:57:45 +0100

    completed 4.4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) return 0;
+  if (blogs.length === 1) return blogs[0].likes;
+  return blogs.reduce((acc, blog) => {
+    return acc + blog.likes;
+  }, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
