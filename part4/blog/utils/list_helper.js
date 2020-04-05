b43db127a3aec2bef06340d544f72e1ccaa365a0
adam <20361733+rzencoder@users.noreply.github.com>
2020-04-05 16:08:41 +0100

    completed 4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -3,14 +3,24 @@ const dummy = (blogs) => {
 };
 
 const totalLikes = (blogs) => {
-  if (blogs.length === 0) return 0;
+  if (!blogs.length) return 0;
   if (blogs.length === 1) return blogs[0].likes;
   return blogs.reduce((acc, blog) => {
     return acc + blog.likes;
   }, 0);
 };
 
+const favouriteBlog = (blogs) => {
+  if (!blogs.length) return "no blogs submitted";
+  if (blogs.title) return blogs;
+  const favBlog = blogs.reduce((acc, val) => {
+    return acc.likes > val.likes ? acc : val;
+  });
+  return favBlog;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favouriteBlog,
 };
