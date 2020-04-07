b14a79108abadd4c20cb57c956de54ab324501f8
adam <20361733+rzencoder@users.noreply.github.com>
2020-04-07 16:59:10 +0100

    completed 4.15

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -7,26 +7,28 @@ const config = require("./utils/config");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 
 logger.info("connecting to", config.MONGODB_URI);
 
 mongoose
-  .connect(config.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    logger.info("connected to MongoDB");
-  })
-  .catch((error) => {
-    logger.error("error connection to MongoDB:", error.message);
-  });
+	.connect(config.MONGODB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		logger.info("connected to MongoDB");
+	})
+	.catch((error) => {
+		logger.error("error connection to MongoDB:", error.message);
+	});
 
 app.use(cors());
 app.use(express.static("build"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use("/api/users", usersRouter);
 app.use("/api/blogs", blogsRouter);
 
 app.use(middleware.unknownEndpoint);
