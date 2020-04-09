e9053571e911ab74fb21c4d56d6bb972b1c10300
adam <20361733+rzencoder@users.noreply.github.com>
2020-04-09 13:26:53 +0100

    completed 4.18

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -8,6 +8,7 @@ const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 logger.info("connecting to", config.MONGODB_URI);
 
@@ -30,6 +31,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/users", usersRouter);
 app.use("/api/blogs", blogsRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
