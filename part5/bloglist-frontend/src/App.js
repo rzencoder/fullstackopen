9ccbdd02dfbb17963946acacfff46a31373f0d89
adam <20361733+rzencoder@users.noreply.github.com>
2020-04-10 17:21:49 +0100

    completed 5.2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -13,6 +13,14 @@ const App = () => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogAppUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+    }
+  }, []);
+
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
@@ -20,12 +28,18 @@ const App = () => {
         username,
         password,
       });
+      window.localStorage.setItem("loggedBlogAppUser", JSON.stringify(user));
       setUser(user);
       setUsername("");
       setPassword("");
     } catch (error) {
       console.log("error");
     }
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+    window.localStorage.removeItem("loggedBlogAppUser");
   };
 
   const loginForm = () => (
@@ -52,9 +66,17 @@ const App = () => {
     </form>
   );
 
+  const renderLogout = () => (
+    <div>
+      <button name="Logout" onClick={handleLogout}>
+        Logout
+      </button>
+    </div>
+  );
+
   return (
     <div>
-      {user === null && loginForm()}
+      {user === null ? loginForm() : renderLogout()}
       <h2>blogs</h2>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
