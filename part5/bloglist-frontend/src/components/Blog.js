05c0562f6360e98955695277756fca6829c89880
adam <20361733+rzencoder@users.noreply.github.com>
2020-04-11 11:31:45 +0100

    completed 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,35 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const [showAll, setShowAll] = useState(false);
 
-export default Blog
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div style={{ display: "flex" }}>
+        <div>{blog.title}</div>
+        <button onClick={() => setShowAll(!showAll)}>
+          {showAll ? "Hide" : "Show"}
+        </button>
+      </div>
+      {showAll && (
+        <>
+          <div>{blog.author}</div>
+          <div style={{ display: "flex" }}>
+            <div>{blog.likes}</div>
+            <button>Like</button>
+          </div>
+          <div>{blog.url}</div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
