c89cbbba546ecb4dd0a31f952e2557b44ac3e5aa
adam <20361733+rzencoder@users.noreply.github.com>
2020-05-13 11:16:53 +0100

    completed 8.2

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -79,9 +79,17 @@ let books = [
 ];
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: String!
+    genres: [String!]!
+  }
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `;
 
@@ -89,6 +97,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 };
 
