8f3d107e5e7d33eea6abf88ae6fcff9bd60b989c
adam <20361733+rzencoder@users.noreply.github.com>
2020-06-19 13:53:42 +0100

    completed 9.19

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -1,18 +1,60 @@
 export interface DiagnosisEntry {
-    code: string,
-    name: string,
-    latin?: string
-}
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+  code: string,
+  name: string,
+  latin?: string
 }
 
 export enum Gender {
-    Male = 'male',
-    Female = 'female',
-    Other = 'other',
+  Male = 'male',
+  Female = 'female',
+  Other = 'other',
 }
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<DiagnosisEntry["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  sickLeave: SickLeave;
+  employerName: string;
+}
+
+export type Entry =
+  | HealthCheckEntry
+  | HospitalEntry
+  | OccupationalHealthcareEntry;
 
 export interface Patient {
   id: string;
@@ -21,10 +63,11 @@ export interface Patient {
   occupation: string;
   gender: Gender;
   dateOfBirth: string;
-  entries: Entry[]}
+  entries: Entry[]
+}
 
-export type PublicPatient = Omit<Patient, 'ssn' | 'entries' >
+export type PublicPatient = Omit<Patient, 'ssn' | 'entries'>;
 
-export type NonSensitivePatientEntry = Omit<PatientEntry, 'ssn'>;
+export type NonSensitivePatientEntry = Omit<Patient, 'ssn'>;
 
-export type NewPatientEntry = Omit<PatientEntry, 'id'>;
+export type NewPatientEntry = Omit<Patient, 'id'>;
