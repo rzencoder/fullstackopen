5cee6f3e51ade205f8333c25e9a67ddf41388156
adam <20361733+rzencoder@users.noreply.github.com>
2020-06-19 10:52:42 +0100

    completed 9.18

--- part9/patientor/src/state/reducer.ts
+++ part9/patientor/src/state/reducer.ts
@@ -3,13 +3,27 @@ import { Patient } from "../types";
 
 export type Action =
   | {
-      type: "SET_PATIENT_LIST";
-      payload: Patient[];
-    }
+    type: "SET_PATIENT_LIST";
+    payload: Patient[];
+  }
   | {
-      type: "ADD_PATIENT";
-      payload: Patient;
-    };
+    type: "ADD_PATIENT";
+    payload: Patient;
+  };
+
+export const setPatientList = (patients: Patient[]): Action => {
+  return {
+    type: 'SET_PATIENT_LIST',
+    payload: patients
+  };
+};
+
+export const addPatient = (newPatient: Patient): Action => {
+  return {
+    type: 'ADD_PATIENT',
+    payload: newPatient
+  };
+};
 
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
